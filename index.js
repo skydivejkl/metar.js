a659f83e4b2eec4925443c555638611c5b17e819
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-18 00:12:54 +0300

    initial

---
+++ index.js
@@ -0,0 +1,193 @@
+
+function asInt(s) {
+    return parseInt(s, 10);
+}
+
+function METAR(metarString) {
+    this.fields = metarString.split(" ");
+    this.i = -1;
+    this.current = null;
+    this.result = {};
+}
+
+METAR.prototype.next = function() {
+    this.i++;
+    return this.current = this.fields[this.i];
+};
+
+METAR.prototype.peek = function() {
+    return this.fields[this.i+1];
+};
+
+METAR.prototype.parseStation = function() {
+    this.next();
+    this.result.station = this.current;
+};
+
+METAR.prototype.parseDate = function() {
+    this.next();
+    var d = new Date();
+    d.setUTCDate(asInt(this.current.slice(0,2)));
+    d.setUTCHours(asInt(this.current.slice(2,4)));
+    d.setUTCMinutes(asInt(this.current.slice(4,6)));
+    this.result.time = d;
+};
+
+METAR.prototype.parseAuto = function() {
+    if (this.peek() === "AUTO") {
+        this.result.auto = true;
+        this.next();
+    }
+};
+
+var variableWind = /^([0-9]{3})V([0-9]{3})$/;
+METAR.prototype.parseWind = function() {
+    this.next();
+    this.result.wind = {};
+
+    var direction = this.current.slice(0,3);
+    if (direction === "VRB") {
+        this.result.wind.direction = "VRB";
+        this.result.wind.variation = true;
+    }
+    else {
+        this.result.wind.direction = asInt(direction);
+    }
+
+    var gust = this.current.slice(5,8);
+    if (gust[0] === "G") {
+        this.result.wind.gust = asInt(gust.slice(1));
+    }
+
+    this.result.wind.speed = asInt(this.current.slice(3,5));
+
+    var unitMatch;
+    if (unitMatch = this.current.match(/KT|MPS|KPH$/)) {
+        this.result.wind.unit = unitMatch[0];
+    }
+    else {
+        throw new Error("Bad wind unit: " + this.current);
+    }
+
+    var varMatch;
+    if (varMatch = this.peek().match(variableWind)) {
+        this.next();
+        this.result.wind.variation = [
+            asInt(varMatch[1]),
+            asInt(varMatch[2]),
+        ];
+    }
+};
+
+
+METAR.prototype.parseCavok = function() {
+    if (this.peek() === "CAVOK") {
+        this.result.cavok = true;
+        this.next();
+    }
+};
+
+METAR.prototype.parseVisibility = function() {
+    if (this.result.cavok) return;
+    this.next();
+    this.result.visibility = asInt(this.current.slice(0,4));
+    // TODO: Direction too. I've not seen it in finnish METARs...
+};
+
+METAR.prototype.parseRunwayVisibility = function() {
+    if (this.result.cavok) return;
+    if (this.peek().match(/^R[0-9]+/)) {
+        this.next();
+        // TODO: Parse it. I've not seen it in finnish METARs...
+    }
+};
+
+
+var weather = {
+    specifier: ["-", "+", "VC", "RE"],
+    nature: ["MI", "BC", "PR", "DR", "BL", "SH", "FZ"],
+    rain: ["DZ", "RA", "SN", "SG", "IC", "PL", "GR", "GS", "UP"],
+    visibility: ["BR", "FG", "FU", "VA", "DU", "SA", "HZ"],
+    other: ["PO", "SQ", "FC", "SS", "DS"],
+    all: {}
+};
+
+[
+    weather.specifier,
+    weather.nature,
+    weather.rain,
+    weather.visibility,
+    weather.other
+].forEach(function(attributes) {
+    attributes.forEach(function(attr) {
+        weather.all[attr] = attr;
+    });
+});
+
+function sliceWeatherAttribute(s) {
+    return weather.all[s.slice(0, 1)] || weather.all[s.slice(0, 2)];
+}
+
+METAR.prototype.parseWeather = function() {
+    if (this.result.cavok) return;
+    if (!sliceWeatherAttribute(this.peek())) return;
+    this.next();
+    this.result.weather = [];
+    this.parseWeatherAttribute();
+};
+
+METAR.prototype.parseWeatherAttribute = function() {
+    var attr = sliceWeatherAttribute(this.current);
+    if (attr) {
+        this.result.weather.push(attr);
+        this.current = this.current.slice(attr.length);
+        this.parseWeatherAttribute();
+    }
+};
+
+
+var clouds = {
+    SKC: true,
+    CLR: true,
+    NSC: true,
+    FEW: true,
+    SCT: true,
+    BKN: true,
+    OVC: true
+};
+
+METAR.prototype.parseClouds = function() {
+    if (this.result.cavok) return;
+    var type = this.peek().slice(0,3);
+    if (!clouds[type]) return;
+
+    this.next();
+    this.result.clouds = (this.result.clouds || []);
+
+    var height = this.current.slice(3,6);
+
+    this.result.clouds.push({
+        type: type,
+        height: height ? asInt(height)*100 : null
+    });
+
+    this.parseClouds();
+};
+
+METAR.prototype.parse = function() {
+    this.parseStation();
+    this.parseDate();
+    this.parseAuto();
+    this.parseWind();
+    this.parseCavok();
+    this.parseVisibility();
+    this.parseWeather();
+    this.parseClouds();
+};
+
+
+module.exports = function(metarString) {
+    var m = new METAR(metarString);
+    m.parse();
+    return m.result;
+};
---
+++ test/parser_test.js
@@ -0,0 +1,117 @@
+/*global it:true, describe:true */
+var assert = require("assert");
+var parseMetar = require("../index");
+
+
+describe("METAR parser", function() {
+
+    it("can parse station", function(){
+        var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+        assert.equal("EFJY", m.station);
+    });
+
+    it("can parse time of observation", function(){
+        var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+        assert.equal(20, m.time.getHours());
+        assert.equal(50, m.time.getMinutes());
+    });
+
+    it("can parse auto", function(){
+        var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+        assert.equal(true, m.auto);
+    });
+
+    it("can parse metar without auto", function() {
+        var m = parseMetar("EFJY 171750Z 29007KT CAVOK 15/12 Q1006");
+        assert.equal(290, m.wind.direction);
+        assert(!m.auto);
+    });
+
+    it("can parse CAVOK", function(){
+        var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+        assert.equal(true, m.cavok);
+    });
+
+    describe("for winds", function() {
+        it("can parse direction", function(){
+            var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+            assert.equal(290, m.wind.direction);
+        });
+        it("can parse speed", function(){
+            var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+            assert.equal(7, m.wind.speed);
+            assert.equal("KT", m.wind.unit);
+        });
+        it("can parse variable directions", function(){
+            var m = parseMetar("EFHF 171820Z AUTO 29007KT 240V330 CAVOK 15/11 Q1010");
+            assert.deepEqual([240, 330], m.wind.variation);
+        });
+        it("can parse small variable directions", function(){
+            var m = parseMetar("EFVA 171850Z AUTO VRB02KT CAVOK 15/11 Q1008");
+            assert.equal(2, m.wind.speed);
+            assert.equal(true, m.wind.variation);
+            assert.equal("VRB", m.wind.direction);
+        });
+        it("can parse gusts", function(){
+            var m = parseMetar("EFVA 171850Z AUTO 24028G42KT CAVOK 15/11 Q1008");
+            assert.equal(28, m.wind.speed);
+            assert(!m.wind.variation);
+            assert.equal(240, m.wind.direction);
+            assert.equal(42, m.wind.gust);
+        });
+
+        it("can MPH speed", function() {
+            var m = parseMetar("ULLI 172030Z 23004MPS 9999 -SHRA SCT022CB BKN043 OVC066 13/10 Q1010 NOSIG");
+            assert.equal(4, m.wind.speed);
+            assert.equal("MPS", m.wind.unit);
+        });
+    });
+
+    describe("for visibility", function() {
+        it("parses no visibility for CAVOK", function(){
+            var m = parseMetar("EFJY 171750Z AUTO 29007KT CAVOK 15/12 Q1006");
+            assert(!m.visibility);
+        });
+        it("can parse visibility", function(){
+            var m = parseMetar("EFET 171920Z AUTO 04007KT 010V070 9999 OVC035 09/05 Q1009");
+            assert.equal(9999, m.visibility);
+        });
+    });
+
+    describe("for weather conditions", function() {
+        it("can parse it", function() {
+            var m = parseMetar("EFKI 171950Z 00000KT 9999 MIFG FEW012 SCT200 10/10 Q1006");
+            assert.deepEqual(["MI", "FG"], m.weather);
+        });
+        it("can parse single attribute weather", function() {
+            var m = parseMetar("EFKI 172020Z AUTO 00000KT 2600 BR SKC 09/09 Q1006");
+            assert.deepEqual(["BR"], m.weather);
+        });
+        it("can parse three attribute weather", function() {
+            var m = parseMetar("ULLI 172030Z 23004MPS 9999 -SHRA SCT022CB BKN043 OVC066 13/10 Q1010 NOSIG");
+            assert.deepEqual(["-", "SH", "RA"], m.weather);
+        });
+    });
+
+    describe("for clouds", function() {
+        it("can parse single cloud level", function(){
+            var m = parseMetar("EFET 171920Z AUTO 04007KT 010V070 9999 OVC035 09/05 Q1009");
+            assert.deepEqual([{ type: "OVC", height: 3500 }], m.clouds);
+        });
+        it("can parse multiple cloud levels", function(){
+            var m = parseMetar("EFVR 171950Z AUTO 27006KT 220V310 9999 FEW012 SCT015 BKN060 13/12 Q1006");
+            assert.deepEqual([
+                { type: "FEW", height: 1200 },
+                { type: "SCT", height: 1500 },
+                { type: "BKN", height: 6000 }
+            ], m.clouds);
+        });
+
+        it("can parse without height", function() {
+            var m = parseMetar("EFKI 172020Z AUTO 00000KT 2600 BR SKC 09/09 Q1006");
+            assert.deepEqual([{"type":"SKC","height": null}], m.clouds);
+        });
+
+    });
+
+});
