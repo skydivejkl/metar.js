4d7f40f34facaf81c3dc4adffa3bbe4ca03a706f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-10 13:20:05 +0300

    Add --force-yes

--- ci.sh
+++ ci.sh
@@ -5,6 +5,6 @@ set -eu
 sudo apt-get install python-software-properties
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs -y --force-yes
 npm install
 npm test
