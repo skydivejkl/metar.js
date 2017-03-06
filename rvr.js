1962790b90c7796cdabdf681d297861b15074bfb
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-03-06 17:20:13 +0000

    prettiers

--- rvr.js
+++ rvr.js
@@ -1,50 +1,44 @@
 (function() {
+    var re = /(R\d{2})([L|R|C])?(\/)([P|M])?(\d+)(?:([V])([P|M])?(\d+))?([N|U|D])?(FT)?/g;
 
-var re = /(R\d{2})([L|R|C])?(\/)([P|M])?(\d+)(?:([V])([P|M])?(\d+))?([N|U|D])?(FT)?/g; 
-
-function RVR(rvrString) {
-    this.result = {};
-    this.rvrString = rvrString;
-    this.parse();
-}
-
-RVR.prototype.parse = function() {
-
-    var matches;
-
-    while ((matches = re.exec(this.rvrString)) != null) {
-
-        if (matches.index === re.lastIndex) {
-            re.lastIndex++;
-        }
-
-        this.result = {
-            "runway": matches[1],
-            "direction": matches[2],
-            "seperator": matches[3],
-            "minIndicator": matches[4],
-            "minValue": matches[5],
-            "variableIndicator": matches[6],
-            "maxIndicator": matches[7],
-            "maxValue": matches[8],
-            "trend": matches[9],
-            "unitsOfMeasure": matches[10]
-        }
+    function RVR(rvrString) {
+        this.result = {};
+        this.rvrString = rvrString;
+        this.parse();
     }
 
-};
+    RVR.prototype.parse = function() {
+        var matches;
 
-function parseRVR(rvrString) {
-    var m = new RVR(rvrString);
-    m.parse();
-    return m.result;
-}
+        while ((matches = re.exec(this.rvrString)) != null) {
+            if (matches.index === re.lastIndex) {
+                re.lastIndex++;
+            }
 
-if (typeof module !== "undefined") {
-    module.exports = parseRVR;
-}
-else if (typeof window !== "undefined") {
-    window.parseMETAR = parseMETAR;
-}
+            this.result = {
+                runway: matches[1],
+                direction: matches[2],
+                seperator: matches[3],
+                minIndicator: matches[4],
+                minValue: matches[5],
+                variableIndicator: matches[6],
+                maxIndicator: matches[7],
+                maxValue: matches[8],
+                trend: matches[9],
+                unitsOfMeasure: matches[10],
+            };
+        }
+    };
 
-}());
+    function parseRVR(rvrString) {
+        var m = new RVR(rvrString);
+        m.parse();
+        return m.result;
+    }
+
+    if (typeof module !== "undefined") {
+        module.exports = parseRVR;
+    } else if (typeof window !== "undefined") {
+        window.parseMETAR = parseMETAR;
+    }
+})();
